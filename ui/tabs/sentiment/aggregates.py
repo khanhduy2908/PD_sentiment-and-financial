7b90e49455d95a5701f1fab9b40abc85a13e04bc
Duy Ngo <ngohoangkhanhduy.work@gmail.com>
2025-11-07 13:45:25 +0700

    Delete ui directory

--- ui/tabs/sentiment/aggregates.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-def render(_:object):
-    st.info('Aggregated sentiment will appear here.')
--- ui/tabs/sentiment/news.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-def render(_:object):
-    st.info('Sentiment data not connected yet.')
